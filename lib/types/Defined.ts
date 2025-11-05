c3eee474859d19dc9badd6682f831118e8584334
rollcodejs <account@rollcode.cn>
2025-11-05 10:32:48 +0800

    feat: add luckcanvas

--- lib/types/Defined.ts
+++ lib/types/Defined.ts
@@ -2,11 +2,37 @@ export type Meta = {
   name: string;
   label: string;
   actions: Action[];
-  setters: any;
+  setters: Record<string, Setter>;
+};
+
+type Setter = {
+  type:
+    | "Text"
+    | "Date"
+    | "Group"
+    | "GroupArray"
+    | "Resource"
+    | "Color"
+    | "Switch"
+    | "Select"
+    | "CustomEnum";
+  label: string;
+  default?: any;
 };
 
 export type Action = {
   name: string;
   label: string;
   dataType: "json" | "string" | "boolean" | "number";
+};
+
+export type GroupArray = {
+  value: any[];
+  schema: Record<
+    string,
+    {
+      type: string;
+      label: string;
+    }
+  >;
 };
